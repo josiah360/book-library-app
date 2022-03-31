b98fd95dd086e6646900bf32c146a9d566de0c00
Josiah Olajide <josiaholajide@gmail.com>
2022-03-31 15:17:35 +0100

    Implement a function that displays books in the library

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const bookShelf = document.querySelector('.book-shelf');
+
 const library = []
 
 function Book(author, title, pages, isRead) {
@@ -16,4 +18,24 @@ function addBookToLibrary(author, title, pages, isRead) {
     const newBook = new Book(author, title, pages, isRead);
     library.push(newBook)
 }
+
+
+function displayBooks(element, array) {
+    const html = array.map(book => {
+        return `<li class='book'>
+                <div class="cover"></div>
+                <p class="title">${book.title}</p>
+                <p class="author">by ${book.author}</p>
+                <p class="info">
+                    <span class="pages">${book.pages} pages</span>
+                    <label><input type="checkbox">Mark as read</label>
+                </p>
+                <img src="img/bin-icon.svg">
+                </li>`;
+        
+    })
+    element.innerHTML = html.join('');
+}
+
+
 
